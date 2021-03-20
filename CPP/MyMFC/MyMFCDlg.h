fcc986495c5c2ebf8b95cebf9b4b8969a5c111fa
Kwon1995-2 <67689901+Kwon1995-2@users.noreply.github.com>
2021-03-20 21:09:04 +0900

    Add files via upload

---
+++ CPP/MyMFC/MyMFCDlg.h
@@ -0,0 +1,46 @@
+﻿
+// MyMFCDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CMyMFCDlg 대화 상자
+class CMyMFCDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CMyMFCDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MYMFC_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedBtnload();
+	afx_msg void OnBnClickedBtninsert();
+	afx_msg void OnBnClickedBtndelete();
+	afx_msg void OnBnClickedBtnmodify();
+	afx_msg void OnBnClickedBtnsave();
+	CString m_strName;
+	CString m_strEmail;
+	CString m_strMobileTel;
+	CString m_strOfficeTel;
+	CString m_strHomeTel;
+	CString m_strBirth;
+};
---
+++ CPP/MyMFC/resource.h
@@ -0,0 +1,33 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// MyMFC.rc에서 사용되고 있습니다.
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_MYMFC_DIALOG                102
+#define IDR_MAINFRAME                   128
+#define IDC_BTNLOAD                     1001
+#define IDC_BTNINSERT                   1002
+#define IDC_BTNDELETE                   1003
+#define IDC_BTNSAVE                     1005
+#define IDC_EDIT1                       1008
+#define IDC_LIST3                       1011
+#define IDC_EDIT2                       1013
+#define IDC_EDIT7                       1013
+#define IDC_EDIT3                       1014
+#define IDC_EDIT4                       1015
+#define IDC_EDIT5                       1016
+#define IDC_EDIT6                       1017
+#define IDC_BTNMODIFY                   1018
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1019
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
