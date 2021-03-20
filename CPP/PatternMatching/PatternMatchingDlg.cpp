a7003d2de961b0aaa5c8943a6435c5c09d96f7d1
Kwon1995-2 <67689901+Kwon1995-2@users.noreply.github.com>
2021-03-20 21:12:43 +0900

    Add files via upload

---
+++ CPP/PatternMatching/PatternMatchingDlg.cpp
@@ -0,0 +1,214 @@
+﻿
+// PatternMatchingDlg.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "PatternMatching.h"
+#include "PatternMatchingDlg.h"
+#include "afxdialogex.h"
+#include <string>
+using namespace std;
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// 응용 프로그램 정보에 사용되는 CAboutDlg 대화 상자입니다.
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTBOX };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+// 구현입니다.
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CPatternMatchingDlg 대화 상자
+
+
+
+CPatternMatchingDlg::CPatternMatchingDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_PATTERNMATCHING_DIALOG, pParent)
+	, m_strPString(_T("abacab"))
+	, m_nType(0)
+	, m_strText(_T("abacaabaccabacabaabbabacaabaccabacabaabbabacaabaccabacabaabb"))
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CPatternMatchingDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Text(pDX, IDC_EDIT1, m_strPString);
+
+	DDX_Control(pDX, IDC_LIST2, m_ctrlList2);
+	DDX_Radio(pDX, IDC_RADIO1, m_nType);
+	DDX_Text(pDX, IDC_EDIT2, m_strText);
+}
+
+BEGIN_MESSAGE_MAP(CPatternMatchingDlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_BN_CLICKED(IDC_BTNSEARCH, &CPatternMatchingDlg::OnBnClickedBtnsearch)
+	ON_BN_CLICKED(IDCANCEL, &CPatternMatchingDlg::OnBnClickedCancel)
+END_MESSAGE_MAP()
+
+
+// CPatternMatchingDlg 메시지 처리기
+
+BOOL CPatternMatchingDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// 시스템 메뉴에 "정보..." 메뉴 항목을 추가합니다.
+
+	// IDM_ABOUTBOX는 시스템 명령 범위에 있어야 합니다.
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != nullptr)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// 이 대화 상자의 아이콘을 설정합니다.  응용 프로그램의 주 창이 대화 상자가 아닐 경우에는
+	//  프레임워크가 이 작업을 자동으로 수행합니다.
+	SetIcon(m_hIcon, TRUE);			// 큰 아이콘을 설정합니다.
+	SetIcon(m_hIcon, FALSE);		// 작은 아이콘을 설정합니다.
+
+	// TODO: 여기에 추가 초기화 작업을 추가합니다.
+
+	return TRUE;  // 포커스를 컨트롤에 설정하지 않으면 TRUE를 반환합니다.
+}
+
+void CPatternMatchingDlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// 대화 상자에 최소화 단추를 추가할 경우 아이콘을 그리려면
+//  아래 코드가 필요합니다.  문서/뷰 모델을 사용하는 MFC 애플리케이션의 경우에는
+//  프레임워크에서 이 작업을 자동으로 수행합니다.
+
+void CPatternMatchingDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 클라이언트 사각형에서 아이콘을 가운데에 맞춥니다.
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 아이콘을 그립니다.
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+// 사용자가 최소화된 창을 끄는 동안에 커서가 표시되도록 시스템에서
+//  이 함수를 호출합니다.
+HCURSOR CPatternMatchingDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+
+
+void CPatternMatchingDlg::OnBnClickedBtnsearch()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	/*if (m_nType == 0) {
+		string str = "";
+		PatternMatch* pm = new Brute_Force;
+		str += "BF:";
+		int idx = pm->patternmatch((LPSTR)(LPCTSTR)m_strText, (LPSTR)(LPCTSTR)m_strText);
+		str += to_string(idx);
+		m_ctrlList2.AddString((LPCTSTR)str.c_str());
+	}
+	else {
+
+	}*/
+	UpdateData(TRUE);
+	m_ctrlList2.ResetContent();
+	PatternMatch* pm;
+	if (m_nType == 0) pm = new Brute_Force;
+	else pm = new Boyer_Moore;
+
+	const char* pText = (LPCTSTR)m_strText;
+	const char* pPtrn = (LPCTSTR)m_strPString;
+
+	int offset = 0;
+	CString str;
+	while (1)
+	{
+		int i = pm->patternmatch(pText + offset, pPtrn); //pText에서 offset을 더한 번째의 원소를 호출
+		if (i < 0) break; //같은 것이 없다는 뜻
+
+		i += offset;
+		str.Format("%s%d", pm->getTypeName(), i); // BF: or BM:
+		m_ctrlList2.AddString(str);
+
+		offset = i + strlen(pPtrn); //찾게 된 같은 문자열에서 패턴 문자열을 더한, 즉 아직 비교하지 않은 문자열부터 비교 시작
+	}
+	delete pm;
+}
+
+
+
+void CPatternMatchingDlg::OnBnClickedCancel()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	CDialogEx::OnCancel();
+}
---
+++ CPP/PatternMatching/PatternMatchingDlg.h
@@ -0,0 +1,114 @@
+﻿
+// PatternMatchingDlg.h: 헤더 파일
+//
+
+#pragma once
+#include <vector>
+
+// CPatternMatchingDlg 대화 상자
+class CPatternMatchingDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CPatternMatchingDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_PATTERNMATCHING_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	CString m_strPString;
+	
+	CListBox m_ctrlList2;
+	afx_msg void OnBnClickedBtnsearch();
+	afx_msg void OnBnClickedCancel();
+	BOOL m_nType;
+	CString m_strText;
+};
+
+class PatternMatch {
+public:
+	virtual int patternmatch(const char* pText, const char* pPtrn) = 0;
+	virtual CString getTypeName() = 0;
+};
+
+class Brute_Force : public PatternMatch {
+public:
+	int patternmatch(const char* pText, const char* pPtrn) {
+		//pText : n개의 문자를 가진 문자열 T
+	//pPtrn : m개의 문자를 가진 문자열 P(패턴)
+		int n = (int)strlen(pText);//sizeof(*pText); //Text String 
+		int m = (int)strlen(pPtrn);//sizeof(*pPtrn); //Pattern String
+		int j;
+		for (int i = 0; i < n - m; i++) {
+			j = 0;
+			while ((j < m) && (pText[i+j] == pPtrn[j])) {
+				//pText[i+j]
+				//i++; -> i가 움직이면 비교하지 않는 값이 생겨버림
+				j = j + 1;
+				if (j == m) return i;//-m;
+				//else break;
+			}
+		}
+		//"pPtrn과 일치하는 pText의 문자열이 없다."
+		return -1;
+	}
+	CString getTypeName() { return CString("BF:"); }
+};
+class Boyer_Moore : public PatternMatch {
+public:
+	std::vector<int> buildLastFunction(const char* pPtrn) {
+		int lenPtrn = (int)strlen(pPtrn);
+
+		const int N_ASCII = 128;
+		std::vector<int> last(N_ASCII); //128개의 원소를 가지는 vector last 생성
+		for (int i = 0; i < N_ASCII; i++) {
+			last[i] = -1; //128칸에 모두 -1로 초기화
+		}
+		for (int i = 0; i < lenPtrn; i++) {
+			last[pPtrn[i]] = i; //if(pPtrn="abc") -> last(97, 98, 99)번째에 0, 1, 2가 들어감
+		}
+		return last;
+	}
+
+	int patternmatch(const char* pText, const char* pPtrn)
+	{
+		int lenText = (int)strlen(pText); //10칸이라 가정
+		int lenPtrn = (int)strlen(pPtrn); //3칸이라 가정
+
+		std::vector<int> last = buildLastFunction(pPtrn); //128칸 짜리가 last로 들어온다.
+		int i = lenPtrn - 1; //2칸
+		if (lenText - 1 < i) { // 9 < 2 -> 패턴보다 작으면 같지 않으니까 -1 반환
+			return -1;
+		}
+
+		int j = lenPtrn - 1;  //j=2칸
+		do { //처음에 무조건 실행->이후는 while문과 동일
+			if (pPtrn[j] == pText[i]) //pPtrn[2] == pText[2] -> if(if or else) 계속 if문 안으로 들어오면 같은 문자열의 시작점 반환할 것
+				if (j == 0) return i;
+				else { i--; j--; }
+			else {
+				i = i + lenPtrn - min(j, 1 + last[pText[i]]); //1,2 + 3부터 - (j(1,2) or -1 or 0, 1, 2)
+				j = lenPtrn - 1; //j는 여전히 2로 세팅됨
+			}
+		} while (i <= lenText - 1); //i<=9 :: i가 전체 비교 평문보다 작을 때 while문 돈다.
+		return -1;
+	}
+
+	CString getTypeName() { return CString("BM:"); }
+};
