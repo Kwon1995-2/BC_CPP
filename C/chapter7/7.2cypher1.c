05528314389304f153d9b01e1c6944fabc741d49
Kwon1995-2 <67689901+Kwon1995-2@users.noreply.github.com>
2021-03-20 20:56:22 +0900

    Add files via upload

---
+++ C/chapter7/7.2cypher1.c
@@ -0,0 +1,22 @@
+//°á°ú°¡ ±³¼ö´Ô ¸»¾¸ÀÌ¶û ´Ù¸¥ ¿¹Á¦
+#include <stdio.h>
+#define SPACE ' '
+
+int main(void)
+{
+	char ch;
+
+	ch = getchar();
+	while (ch != '\n')
+	{
+	/*	if (ch == SPACE)
+			putchar(ch);
+		else
+			putchar(ch+1); */
+		putchar(ch);
+		ch = getchar();
+	}
+	putchar(ch);
+
+	return 0;
+}
