9dc30d589960450821426525e4e25158e7d3ef0b
Kwon1995-2 <67689901+Kwon1995-2@users.noreply.github.com>
2021-03-20 21:33:15 +0900

    Add files via upload

---
+++ CPP/ContactManager3/ContactManager3.h
@@ -0,0 +1,32 @@
+﻿
+// ContactManager3.h: PROJECT_NAME 애플리케이션에 대한 주 헤더 파일입니다.
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH에 대해 이 파일을 포함하기 전에 'pch.h'를 포함합니다."
+#endif
+
+#include "resource.h"		// 주 기호입니다.
+
+
+// CContactManager3App:
+// 이 클래스의 구현에 대해서는 ContactManager3.cpp을(를) 참조하세요.
+//
+
+class CContactManager3App : public CWinApp
+{
+public:
+	CContactManager3App();
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+
+// 구현입니다.
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CContactManager3App theApp;
---
+++ CPP/ContactManager3/ContactManager3Dlg.h
@@ -0,0 +1,58 @@
+﻿
+// ContactManager3Dlg.h: 헤더 파일
+//
+
+#pragma once
+
+#include <vector>
+#include "MyHash.h"
+
+// CContactManager3Dlg 대화 상자
+class CContactManager3Dlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CContactManager3Dlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+	~CContactManager3Dlg();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_CONTACTMANAGER3_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+private:
+	void displayHashtoList();
+	void tockenstring(vector<string>& vec, char* pstr, size_t len);
+	MyHash* hash;
+	int bucketSize;
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	CString m_strName;
+	CString m_strMobileTel;
+	CString m_strOfficeTel;
+	CString m_strHomeTel;
+	CString m_strEmail;
+	CString m_strBirth;
+	CListBox m_ctrlList;
+	CListBox m_ctrlList2;
+	afx_msg void OnBnClickedDataload();
+	afx_msg void OnBnClickedDatainsert();
+	afx_msg void OnBnClickedDatadelete();
+	afx_msg void OnBnClickedDatamodify();
+	afx_msg void OnBnClickedDatasave();
+	afx_msg void OnBnClickedCancel();
+	afx_msg void OnSelchangeList1();
+};
